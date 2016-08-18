5b9609d4a392ed9b6979bc581bfbb6f6a65c6f50
omkartech <oshetye152@gmail.com>
2016-08-18 23:20:48 +0530

    test

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,7 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/tourse/';
+$config['base_url'] = 'http://localhost/insight_tours/';
 
 /*
 |--------------------------------------------------------------------------
